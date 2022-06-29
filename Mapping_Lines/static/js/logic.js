5333abe87fa6b38e51de4d49c4e88a0475cbcacc
Scott Miller <102341406+Bulzeye89@users.noreply.github.com>
2022-06-29 15:20:18 -0700

    module 13.4.3 complete

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,57 +2,47 @@
 console.log("working");
 
 // Create the map object with options
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.0902, -95.7129], 5);
 
-//Alternatively we could create map object with a center and zoom level as seen below
-//Useful for when adding multiple til layers or background images
-// let map = L.map("mapid", {
-//     center: [
-//         40.7, -94.5
-//     ],
-//     zoom: 4
-// });
+//Coordinates for each point to be used in the polyline
+let line = [
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
 
 
-//Get data from cities.js
-let cityData = cities;
+//Create a polyline using the line coordinates and make the line red
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: "4, 10"
+  
 
-  //Loop through the cities array and create one marker for each city.
-  cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        color: 'orange',
-        lineweight: 4
-    })
-    .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population " +city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-  });
-
+  
+}).addTo(map);
 
 
 //Create the tile layer that will be the background of the map w/ Leaflet documentation style
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: API_KEY
-// });
-// // Then we add our 'graymap' tile layer to the map.
-// streets.addTo(map);
 
-// Alternatively do the same as bove but with mapbox style documentation
 //https://docs.mapbox.com/help/glossary/static-tiles-api/
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  //id: "mapbox/streets-v11", //Mapbox Streets
+  //id: "mapbox/outdoors-v11", //Mapbox Outdoors
+  id: "mapbox/light-v10", //Mapbox Light
+  //id: "mapbox/dark-v10", //Mapbox Dark
+  //id: "mapbox/satellite-v9", //Mapbox Satellite
+  // id: "mapbox/satellite-streets-v11", //Mapbox Satellite Streets
+  //id: "mapbox/navigation-day-v1", //Mapbox Navigation Day
+  // id: "mapbox/navigation-night-v1", //Mapbox Navigation Night
+  accessToken: API_KEY
+}).addTo(map);
 
 
